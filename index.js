2fec4d5affd6a5b303be451aab8f2d02d85ece26
Alexander Kuznetsov <sasha@monterosa.co.uk>
2015-03-11 15:33:58 +0000

    Exposing built-in hooks

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 module.exports =
 {
 	compose: require('./lib/compose.js'),
+
+	hooks: {
+		copy: require('./lib/hooks/copy.js'),
+		gitignore: require('./lib/hooks/gitignore.js')
+	},
+
 	pack: require('./lib/pack.js')
 }
